579eb1026321b8b7e794061b1d2b9585e026ce5d
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-28 08:11:42 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,16 @@
+class Circle
+{
+    public static void main(String args[])
+    {
+        int r;
+        double pi= 3.14, area;
+        Scanner s = new scanner (System.in);
+        System.out.println("enter R value:");
+        r=s.nextInt();
+        area= pi * r * r;
+        System.out.println("area of circle:"+area);
+        }
+
+2+275
+
+22
