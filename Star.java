f240fe698beb880594e450ef20a2c490860e2882
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-28 08:27:38 +0530

    Add files via upload

---
+++ Star.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Star {
+    public static void main(String[] args) {
+        int n,i,j;
+	Scanner s = new Scanner(System.in);
+	System.out.println("enter n value:");
+	n=s.nextInt();
+	for(i=1; i < n; i++){
+	for(j=1; j <= i; j++){
+	System.out.println(j + " " );
+			}
+		System.out.println( );
+		}
+	}	
+}
