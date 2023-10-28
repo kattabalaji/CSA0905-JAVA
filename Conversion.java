4a9576303ffeecfdd2bc417aecf2574b2f6a8004
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-28 08:12:00 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Convert
+{
+	public class void main(String args[])
+	{	
+		int value;
+		string binary;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("decimal num:");
+		value=sc.nextInt();
+		binary=integer.toBinaryString(value);
+		System.out.println(binary);
+	}
+}
