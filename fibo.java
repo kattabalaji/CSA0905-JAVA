288f1b3d69235103920d6958e599b20445da29c3
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-28 08:13:58 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Fibo
+{
+    public static void main(String args[])
+    {
+        int a=0,b=2,c,i,sum=0,n;
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter a number:");
+        n=s.nextInt();
+	for(i=0;i<n;i++)
+	{
+	n3=n1+n2;
+	System.out.println(" "+n3);
+	sum+=n3;
+	n1=n2;
+	n2=n3;
+	if(i%2!=0)
+    {	
+	sum+=c;
+	System.out.println("sum="+sum);
+}
+}
+}
+}
