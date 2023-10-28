7e46ef085043c546a7576bc2f6b85f764dee6c0f
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-28 08:13:11 +0530

    Add files via upload

---
+++ Convert.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class DecimalToBinaryAndOctal
+{
+	public class void main(String[] args)
+	{	
+		int value;
+		string binary;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("decimal num:");
+		value=sc.nextInt();
+		binary=integer.toBinaryString(value);
+		System.out.println(binary);
+		octal=integer.toOcctalString(value);
+		System.out.println(binary);
+	}
+}
