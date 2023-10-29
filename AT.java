2ea07810096b7dd73157cdb7dd1ccb14de26da63
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-29 11:52:57 +0530

    Add files via upload

--- AT.java
+++ AT.java
@@ -1,11 +1,11 @@
 class AT
 {
-public static void main(String arg[])
-{
-	int b,h;
-	double A;
-	b=4;h=6;
-	A=(b*h)/2;
-	System.out.println("Area="+A);
-}
+	public static void main(String arg[])
+	{
+		int b,h;
+		double A;
+		b=4;h=6;
+		A=(b*h)/2;
+		System.out.println("Area="+A);
+	}
 }
