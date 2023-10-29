736828baa0a76c90493c516a661832c8d0f30207
balaji <113337367+kattabalaji@users.noreply.github.com>
2023-10-29 11:19:41 +0530

    Add files via upload

---
+++ Squarecube.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class SquareCube
+{
+public static void main(String args[])
+{
+int sq,cube,n;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the number:");
+n=s.nextInt();
+sq=n*n;
+System.out.println("square of " +n +" is :"+sq);
+cube=n*n*n;
+System.out.println("cube of " +n +" is:"+cube);
+}
+}
